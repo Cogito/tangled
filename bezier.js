5f7c4352ff9761b236cd95e0edc595559db76a7f
Andrew Ardill <andrew.ardill@gmail.com>
2012-02-20 20:34:57 +1100

    Initial commit

---
+++ bezier.js
@@ -0,0 +1,127 @@
+(function() {
+  var Point = function(x, y) {
+    return {
+      x: x,
+      y: y,
+      translate: function(vector) {
+        return Point(
+          this.x + vector.length * Math.cos(vector.angle),
+          this.y + vector.length * Math.sin(vector.angle)
+        );
+      }
+    }
+  };
+  var Vector = function(length, angle) {
+    var rotate = function(radians) {
+      return Vector(this.length, this.angle + radians); // TODO: might try to keep new angle in the range [-PI:PI] later
+    };
+    var scaleTo = function(len) {
+      return Vector(len, this.angle);
+    };
+    var scale = function(percentage) {
+      return Vector(this.length*percentage, this.angle);
+    };
+    var draw = function(context, start) {
+      context.moveTo(start.x, start.y);
+      context.lineTo(start.translate(this).x, start.translate(this).y);
+    };
+    return {
+      length: length, // pixels?
+      angle: angle, // radians!
+      rotate: rotate,
+      scaleTo: scaleTo,
+      scale: scale,
+      draw: draw
+    };
+  };
+  var Node = function(v1, v2, tail) {
+    var add = function(node) {
+      this.tail = node;
+      return node;
+    };
+    var extend = function(v3) {
+      this.tail = Node(this.v2, v3);
+      return this.tail;
+    };
+    return {
+      v1: v1,
+      v2: v2,
+      tail: tail, // a list of vectors! (max one to start, then two. probably no more)
+      add: add,
+      extend: extend
+    };
+  };
+  var Bezier = function(startPoint, startControlVector, endPoint, endControlVector){
+    var draw = function(context) {
+      context.moveTo(this.start.x, this.start.y);
+      context.bezierCurveTo(this.startCP.x, this.startCP.y, this.endCP.x, this.endCP.y, this.end.x, this.end.y);
+    };
+    return {
+      start: startPoint,
+      startCP: startPoint.translate(startControlVector),
+      end: endPoint,
+      endCP: endPoint.translate(endControlVector),
+      draw: draw
+    }
+  }
+  var bezierPoints = function(p, v1, v2) {
+    var w = 3;
+    var cpInnerScale = 0.7;
+    var cpOuterScale = 0.9;
+    var cpLeftScale = v1.angle - v2.angle < 0 ? cpOuterScale : cpInnerScale;
+    var cpRightScale = v1.angle - v2.angle < 0 ? cpInnerScale : cpOuterScale;
+    var end_point = p.translate(v1).translate(v2);
+    return {
+      bez1: Bezier(
+        p.translate(v1.rotate(-Math.PI / 2).scaleTo(w)),
+        v1.scale(cpLeftScale),
+        end_point.translate(v2.rotate(-Math.PI / 2).scaleTo(w)),
+        v2.rotate(Math.PI).scale(cpLeftScale)
+      ),
+      bez2: Bezier(
+        p.translate(v1.rotate(Math.PI / 2).scaleTo(w)),
+        v1.scale(cpRightScale),
+        end_point.translate(v2.rotate(Math.PI / 2).scaleTo(w)),
+        v2.rotate(Math.PI).scale(cpRightScale)
+      )
+    }
+  };
+  var drawPoints = function(context, start, v1, v2) {
+    var curves = bezierPoints(start, v1, v2);
+    context.strokeStyle = "rgb(0,0,0)";
+    context.beginPath();
+    curves.bez1.draw(context);
+    curves.bez2.draw(context);
+    context.stroke();
+    context.strokeStyle = "rgba(255,0,0,0.5)";
+    context.beginPath();
+    v1.draw(context, start);
+    v2.draw(context, start.translate(v1));
+    context.stroke();
+  };
+  var draw = function() {
+    var canvas = document.getElementById('myCanvas');
+    var start = Point(0, 50);
+    var vectors = Node(Vector(20, 0), Vector(40, 1));
+    vectors.extend(Vector(35,-1))
+      .extend(Vector(15,0))
+      .extend(Vector(25,-0.5))
+      .extend(Vector(15,0.8))
+      .extend(Vector(50,2.4))
+      .extend(Vector(50,2.4))
+      .extend(Vector(50,1.5))
+      .extend(Vector(100,0));
+    if (canvas.getContext){
+      var ctx = canvas.getContext('2d');
+      var p = start;
+      var node = vectors;
+      for(; node; node = node.tail) {
+        drawPoints(ctx, p, node.v1, node.v2);
+        p = p.translate(node.v1).translate(node.v2);
+      }
+    }
+  };
+  $(function() {
+    draw();
+  });
+})();
